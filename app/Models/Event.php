341b96eeccaecc697c376739a4322c039e2e6bf4
jessicaebd <jessica.natalia23oct@gmail.com>
2021-10-30 22:03:16 +0700

    testing Migration

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -2,41 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Event
+class Event extends Model
 {
-    private static $event_list = [
-        [
-            "eventName" => "Paid Promote",
-            "slug" => "paid-promote",
-            "division" => "Dana",
-            "leader" => "Jenita",
-            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, soluta numquam illum quae unde perferendis voluptatem tempore quibusdam, libero beatae impedit delectus necessitatibus. Expedita modi, vero ad blanditiis nam quas earum dolor, cum odit quae iste, impedit optio quo! Nesciunt provident magni libero maiores voluptatem labore nemo reiciendis blanditiis ipsum."
-        ],
-        [
-            "eventName" => "Jasa Isi Kuesioner",
-            "slug" => "jasa-isi-kuesioner",
-            "division" => "Dana",
-            "leader" => "Verlyn",
-            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, soluta numquam illum quae unde perferendis voluptatem tempore quibusdam, libero beatae impedit delectus necessitatibus. Expedita modi, vero ad blanditiis nam quas earum dolor, cum odit quae iste, impedit optio quo! Nesciunt provident magni libero maiores voluptatem labore nemo reiciendis blanditiis ipsum."
-        ],
-        [
-            "eventName" => "Game Night",
-            "slug" => "game-night",
-            "division" => "Dana & Acara",
-            "leader" => "Alex",
-            "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, soluta numquam illum quae unde perferendis voluptatem tempore quibusdam, libero beatae impedit delectus necessitatibus. Expedita modi, vero ad blanditiis nam quas earum dolor, cum odit quae iste, impedit optio quo! Nesciunt provident magni libero maiores voluptatem labore nemo reiciendis blanditiis ipsum."
-        ],
-    ];
-
-    public static function all() 
-    {
-        return collect(self::$event_list);
-    }
-
-    public static function find($slug) 
-    {
-        $events = static::all();
-        return $events->firstWhere('slug', $slug);
-    }
+    use HasFactory;
 }
---
+++ database/migrations/2021_10_30_142717_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('leader');
+            $table->string('by_division');
+            $table->text('exerpt');
+            $table->text('description');
+            $table->timestamp('start_at')->nullable(true);
+            $table->timestamp('end_at')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}

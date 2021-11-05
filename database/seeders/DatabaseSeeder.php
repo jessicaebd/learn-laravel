36391af2cbaf28e8c8ecc3906a36ba4aa61c43c6
jessicaebd <jessica.natalia23oct@gmail.com>
2021-11-05 19:22:57 +0700

    add dta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Division;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(5)->create();
+        User::factory(5)->create();
+
+        // Divison
+        Division::create([
+            'name' => 'Pengurus Inti',
+            'slug' => 'pengurus-inti',
+            'jobdesk' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque nihil officiis temporibus incidunt ea.'
+        ]);
+
+        Division::create([
+            'name' => 'Sie Dana',
+            'slug' => 'sie-dana',
+            'jobdesk' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque nihil officiis temporibus incidunt ea.'
+        ]);
+
+        Division::create([
+            'name' => 'Sie Acara',
+            'slug' => 'sie-acara',
+            'jobdesk' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque nihil officiis temporibus incidunt ea.'
+        ]);
+
+        Division::create([
+            'name' => 'Sie Pubdok',
+            'slug' => 'sie-pubdok',
+            'jobdesk' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque nihil officiis temporibus incidunt ea.'
+        ]);
+
+        Division::create([
+            'name' => 'Sie Content Creator',
+            'slug' => 'sie-content-creator',
+            'jobdesk' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque nihil officiis temporibus incidunt ea.'
+        ]);
+
+        Division::create([
+            'name' => 'Sie Public Relation',
+            'slug' => 'sie-public-relation',
+            'jobdesk' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque nihil officiis temporibus incidunt ea.'
+        ]);
+
+        Division::create([
+            'name' => 'Sie Doa',
+            'slug' => 'sie-doa',
+            'jobdesk' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque nihil officiis temporibus incidunt ea.'
+        ]);
     }
 }

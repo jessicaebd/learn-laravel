f21e837b4fee0a9bf048ae0ed84231974f733d65
jessicaebd <jessica.natalia23oct@gmail.com>
2021-11-10 18:57:43 +0700

    add user registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,19 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function index()
     {
         return view('register.index', [
-            'title' => "Register"
+            'title' => 'Register'
         ]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return request()->all();
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'username' =>   ['required', 'min:3', 'max:20', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:7|max:20'
+        ]);
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        User::create($validated);
+
+        $request->session()->flash('success', 'Registrasi berhasil!');
+
+        return redirect('/login');
     }
 }

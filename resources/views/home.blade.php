a2166d2d74b598c1b8a9a420cebc107e437dc01b
jessicaebd <jessica.natalia23oct@gmail.com>
2021-11-16 22:16:14 +0700

    add features (not finished yet)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,8 @@
     {{-- Sliding Images --}}
     @include('home.carousel')
     {{-- End of Sliding Images --}}
+
+    <div class="container mt-5">
+        @include('home.featurettes')
+    </div>
 @endsection

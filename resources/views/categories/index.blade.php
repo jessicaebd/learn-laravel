9b9ff6226596e980c963d9a19ecca55a8cdb2aea
jessicaebd <jessica.natalia23oct@gmail.com>
2021-12-06 11:12:41 +0700

    formatting

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,20 +7,21 @@
     <div class="container">
         <div class="row">
             @foreach ($categories as $category)
-            <div class="col-md-4 mb-3">
-                <div class="card bg-dark text-white">
-                    <img src="https://source.unsplash.com/800x400?{{ $category->name }}" class="card-img" alt="{{ $category->name }}">
+                <div class="col-md-4 mb-3">
+                    <div class="card bg-dark text-white">
+                        <img src="https://source.unsplash.com/800x400?{{ $category->name }}" class="card-img"
+                            alt="{{ $category->name }}">
 
 
-                    <div class="card-img-overlay d-flex align-items-center p-0">
-                        <h5 class="card-title text-center flex-fill p-2 fs-3" style="background-color: rgba(0,0,0,0.7)">
-                            <a href="/categories/{{ $category->slug }}" class="text-decoration-none">
-                                {{ $category->name }}
-                            </a>
-                        </h5>
+                        <div class="card-img-overlay d-flex align-items-center p-0">
+                            <h5 class="card-title text-center flex-fill p-2 fs-3" style="background-color: rgba(0,0,0,0.7)">
+                                <a href="/categories/{{ $category->slug }}" class="text-decoration-none">
+                                    {{ $category->name }}
+                                </a>
+                            </h5>
+                        </div>
                     </div>
-                  </div>
-            </div>
+                </div>
             @endforeach
         </div>
     </div>
